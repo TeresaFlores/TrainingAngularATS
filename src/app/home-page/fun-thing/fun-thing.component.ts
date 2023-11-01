45eb139346e6815493b7c30c23946a18f6309f92
Gonzalez Medellin <agustin.gonzalez@accenture.com>
2023-11-01 15:44:37 -0600

    TAREA 2

--- src/app/home-page/fun-thing/fun-thing.component.ts
+++ src/app/home-page/fun-thing/fun-thing.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-fun-thing',
   templateUrl: './fun-thing.component.html',
-  styleUrls: ['./fun-thing.component.css']
+  styleUrls: ['./fun-thing.component.css'],
 })
 export class FunThingComponent implements OnInit {
-
-  constructor() { }
+  fact: string;
+  constructor() {}
 
   ngOnInit(): void {
+    this.fact = 'An owl has three eyelids';
   }
-
 }
--- src/app/home-page/todo-list/todo-list.component.ts
+++ src/app/home-page/todo-list/todo-list.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.css']
+  styleUrls: ['./todo-list.component.css'],
 })
 export class TodoListComponent implements OnInit {
-
-  constructor() { }
+  todos: string[];
+  constructor() {}
 
   ngOnInit(): void {
+    this.todos = ['Learn Angular', 'Be good at it', 'profit'];
   }
-
 }

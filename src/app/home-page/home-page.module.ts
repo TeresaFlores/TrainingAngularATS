46970f05b256666994612b8e7ebe25701bd1cbde
Gonzalez Medellin <agustin.gonzalez@accenture.com>
2023-11-02 19:01:53 -0600

    TAREA 3

--- src/app/home-page/home-page.module.ts
+++ src/app/home-page/home-page.module.ts
@@ -3,10 +3,21 @@ import { CommonModule } from '@angular/common';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { TestThingComponent } from './test-thing/test-thing.component';
 import { FunThingComponent } from './fun-thing/fun-thing.component';
+import { PersonsTableComponent } from './persons-table/persons-table.component';
 
 @NgModule({
-  declarations: [TodoListComponent, TestThingComponent, FunThingComponent],
+  declarations: [
+    TodoListComponent,
+    TestThingComponent,
+    FunThingComponent,
+    PersonsTableComponent,
+  ],
   imports: [CommonModule],
-  exports: [TodoListComponent, TestThingComponent, FunThingComponent],
+  exports: [
+    TodoListComponent,
+    TestThingComponent,
+    FunThingComponent,
+    PersonsTableComponent,
+  ],
 })
 export class HomePageModule {}

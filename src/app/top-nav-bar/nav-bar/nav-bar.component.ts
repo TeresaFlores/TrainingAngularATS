ef3b49f07b65b36027b561b9262598b9383ab581
AgustinGzz <tinyaelito@gmail.com>
2023-11-11 23:31:35 -0600

    add navbar

--- src/app/top-nav-bar/nav-bar/nav-bar.component.ts
+++ src/app/top-nav-bar/nav-bar/nav-bar.component.ts
@@ -3,13 +3,24 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.css']
+  styleUrls: ['./nav-bar.component.css'],
 })
 export class NavBarComponent implements OnInit {
+  homeIcon = 'assets/icons/home-icon.svg';
+  ticketIcon = 'assets/icons/ticket-icon.svg';
+  foodIcon = 'assets/icons/food-icon.svg';
+  contactIcon = 'assets/icons/contact-icon.svg';
+  displayMobile = false;
+  activeLink = '/';
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  changeClass() {
+    this.displayMobile = !this.displayMobile;
   }
 
+  setActiveLink(link: string) {
+    this.activeLink = link;
+  }
 }

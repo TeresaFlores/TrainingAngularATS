8c6ebd068fdd073c834814bb34914648986bf7a1
AgustinGzz <tinyaelito@gmail.com>
2023-11-12 01:51:29 -0600

    reiniciar para proyecto final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HomePageModule } from './home-page/home-page.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HomePageModule],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
 })

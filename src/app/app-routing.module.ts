6c9093c92ebd15f29511a93ea4bb59da95c904bb
AgustinGzz <tinyaelito@gmail.com>
2023-11-12 01:35:01 -0600

    add other pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
+import { TicketsPageComponent } from './tickets-page/tickets-page.component';
+import { FoodPageComponent } from './food-page/food-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
 
-const routes: Routes = [{ path: '/', component: HomePageComponent }];
+const routes: Routes = [
+  { path: '/tickets', component: TicketsPageComponent },
+  { path: '/food', component: FoodPageComponent },
+  { path: '/contact', component: ContactPageComponent },
+  { path: '', component: HomePageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

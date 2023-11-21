03e4a0f3fea5973f7329fca0180a914295e6a9c5
Fer Araujo <fer_araujo@live.com.mx>
2023-11-21 00:06:40 -0600

    Home Page Content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,12 @@ import { NgModule, OnInit } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
 import { NavbarModule } from './components/navbar/navbar.module';
 import { RoutingModule } from './routing/routing.module';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent],
   imports: [BrowserModule, NavbarModule, RoutingModule],
   providers: [],
   bootstrap: [AppComponent],
